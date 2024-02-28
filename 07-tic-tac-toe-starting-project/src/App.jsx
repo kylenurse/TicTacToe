e133131e2f8cce40300d0e9cad927fca3ee7a5b8
Kyle Nurse <kyle.nurse@ryerson.ca>
2024-02-28 07:45:48 -0500

    Added Basic Layout

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,10 +1,23 @@
 
 function App() {
-  
 
   return (
-    <p>Coming soon...</p>
-  )
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <li>
+          <span className="player-name">Player 1</span>
+          <span className="player-symbol">X</span>
+          </li>
+          <li>
+          <span className="player-name">Player 1</span>
+          <span className="player-symbol">O</span>
+          </li>
+        </ol>
+        GAMEBOARD
+      </div>
+    </main>
+  );
 }
 
 export default App

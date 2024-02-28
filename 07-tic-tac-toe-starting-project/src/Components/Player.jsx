d68c3da25d5d2abf4d84d31285fa67016b903666
Kyle Nurse <kyle.nurse@ryerson.ca>
2024-02-28 10:15:08 -0500

    same as previous.

--- 07-tic-tac-toe-starting-project/src/Components/Player.jsx
+++ 07-tic-tac-toe-starting-project/src/Components/Player.jsx
@@ -4,13 +4,14 @@ export default function Player ({name, symbol}) {
 
     const [isEditing, setIsEditing] = useState(false);
     function onSelect () {
-        setIsEditing(true);
+        setIsEditing((editing) => !editing);
     }
 
     let playerName = <span className="player-name">{name}</span>;
+    let btnCaption = 'Edit';
 
     if (isEditing) {
-        playerName = <input type="text" required />;
+        playerName = <input type="text" required value={name}/>;
     }
 
     return (
@@ -19,6 +20,6 @@ export default function Player ({name, symbol}) {
             {playerName}
             <span className="player-symbol">{symbol}</span>
         </span>
-        <button onClick={onSelect}>Edit</button>
+        <button onClick={onSelect}>{isEditing? 'Save' : 'Edit'}</button>
     </li>
 );}

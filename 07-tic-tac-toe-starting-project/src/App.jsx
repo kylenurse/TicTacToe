d34711d58e08d8f11110d32e00f3bf08915d9ee7
Kyle Nurse <kyle.nurse@ryerson.ca>
2024-03-04 14:21:41 -0500

    Added player state to allow turns

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,15 +1,23 @@
 import Player from "./Components/Player";
 import GameBoard from "./Components/GameBoard";
+import { useState } from "react";
+
 function App() {
+
+  const [activePlayer, setActivePlayer] = useState('X');
+
+  function handleSelectSquare() {
+    setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
+  }
 
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
-          <Player initialName="player 1" symbol="X" />
-          <Player initialName="player 2" symbol="O" />
+        <ol id="players" className="highlight-player">
+          <Player initialName="player 1" symbol="X" isActive={activePlayer === 'X'}/>
+          <Player initialName="player 2" symbol="O" isActive={activePlayer === 'O'}/>
         </ol>
-        <GameBoard/>
+        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer}/>
       </div>
     </main>
   );
--- 07-tic-tac-toe-starting-project/src/Components/GameBoard.jsx
+++ 07-tic-tac-toe-starting-project/src/Components/GameBoard.jsx
@@ -7,7 +7,7 @@ const initialGameBoard = [
 ];
 
 
-export default function GameBoard() {
+export default function GameBoard({onSelectSquare, activePlayerSymbol}) {
 
     const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
@@ -15,9 +15,10 @@ export default function GameBoard() {
 
         setGameBoard((prevGameBoard) => {
             const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
-            updatedBoard[rowIndex][colIndex] = 'X';
+            updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
             return updatedBoard;
         })
+        onSelectSquare();
     } 
 
     return (<ol id="game-board">

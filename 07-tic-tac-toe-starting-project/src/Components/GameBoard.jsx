52296a13f7acedbd5f0c89ae1b83200d53a6b70e
Kyle Nurse <kyle.nurse@ryerson.ca>
2024-05-02 19:42:38 -0400

    Added win condition and game over screen

--- 07-tic-tac-toe-starting-project/src/Components/GameBoard.jsx
+++ 07-tic-tac-toe-starting-project/src/Components/GameBoard.jsx
@@ -1,39 +1,14 @@
-const initialGameBoard = [
-    [null , null, null],
-    [null , null, null],
-    [null , null, null],
-];
+export default function GameBoard({ onSelectSquare, board }) {
 
 
-export default function GameBoard({ onSelectSquare, turns }) {
-
-    let gameBoard = initialGameBoard;
-    for (const turn of turns) {
-        const { square, player } = turn;
-        const { row, col } = square;
-
-        gameBoard[row][col] = player;
-    }
-
-    // const [gameBoard, setGameBoard] = useState(initialGameBoard);
-
-    // function handleSelectSquare (rowIndex, colIndex, playerSymbol) {
-
-    //     setGameBoard((prevGameBoard) => {
-    //         const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
-    //         updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
-    //         return updatedBoard;
-    //     })
-    //     onSelectSquare();
-    // } 
-
-    return (<ol id="game-board">
-        {gameBoard.map((row, rowIndex) => <li key={rowIndex}>
+    return (
+    <ol id="game-board">
+        {board.map((row, rowIndex) => <li key={rowIndex}>
             
                 <ol>
                     {row.map((playerSymbol, colIndex) => (
                     <li key={colIndex}>
-                        <button onClick={() => onSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
+                        <button onClick={() => onSelectSquare(rowIndex, colIndex)} disabled={playerSymbol != null}> {playerSymbol} </button>
                     </li>))} 
                 </ol>
           
